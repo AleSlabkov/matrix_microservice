f9198748fe9987fb16c2e175716f10b4152c81d2
Uciel Rodriguez <Uciel.Rodriguez@endava.com>
2018-09-19 12:17:50 -0300

    master / defauls roles and claims

--- ClearStar.Microservice.Auth/Data/UserInfo.cs
+++ ClearStar.Microservice.Auth/Data/UserInfo.cs
@@ -8,7 +8,5 @@ namespace ClearStar.Microservice.Auth.Data
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
-        public IList<Guid> Roles { get; set; }
-        public IDictionary<Guid,string> Claims { get; set; }
     }
 }
